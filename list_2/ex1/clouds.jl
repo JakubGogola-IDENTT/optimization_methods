a57c627ce7205fd489e501037871487a13576d2b
JakubGogola <jakubgogola97@gmail.com>
2020-05-15 21:18:05 +0200

    l2ex1

---
+++ list_2/ex1/clouds.jl
@@ -0,0 +1,40 @@
+# Jakub Gogola 236412
+# Lista 2, zadanie 1
+
+using JuMP
+using GLPK
+
+include("./data.jl")
+
+"""
+This function finds optimal path between servers to find infomration about all popoulation's characteristics
+
+model - reference to JuMP model
+Q - array containing information where informations about each characteristic is stored
+T - array containing times required to search every server
+"""
+function find_optimal_search_path(model::Model, Q::Any, T::Array{Int64})
+    m::Int64 = length(Q)
+    n::Int64 = length(T)
+
+    # Vector containing information which servers should be searched (solution)
+    @variable(model, p[1:n] >= 0)
+    
+    # function to minimalize - time for searched servers should be minimal
+    @objective(model, Min, sum(p[j] * T[j] for j in 1:n))
+
+    # constraint - we want to ensure that for each characteristic we used only one server to find it
+    @constraint(model, [i in 1:m], sum(Q[i][j] * p[j] for j in 1:n) == 1)
+
+    optimize!(model)
+end
+
+function run()
+    model = Model()
+    set_optimizer(model, GLPK.Optimizer)
+
+    find_optimal_search_path(model, Q, T)
+
+    println(termination_status(model))
+    println(model)
+end
---
+++ list_2/ex1/data.jl
@@ -0,0 +1,13 @@
+# Vector T contains times which are required to search every server
+T = [2 3 7 1 5 6 9 4 4 5]
+
+# Matrix Q contains info about servers where are stored informations about each characteristic of population
+Q = [
+    [0 0 0 1 0 1 0 1 1 0],
+    [1 0 0 0 0 0 1 0 0 0],
+    [1 1 0 0 0 1 0 1 0 0],
+    [0 1 0 0 0 0 0 0 0 0],
+    [0 0 1 0 1 0 0 1 0 1],
+    [0 0 1 1 0 0 0 0 0 1],
+    [1 0 0 0 0 0 0 0 0 1]
+]
