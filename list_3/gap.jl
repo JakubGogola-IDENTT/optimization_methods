39385142a289788fe4796b95e54f419dd6652f81
JakubGogola <jakubgogola97@gmail.com>
2020-05-31 00:14:37 +0200

    parsing fixed

--- list_3/gap.jl
+++ list_3/gap.jl
@@ -1,13 +1,47 @@
 include("parser.jl")
 
+using JuMP
+using GLPK
+
 function solve_gap(problem_data)
     machines_count, jobs_count, costs, resources, capacities = problem_data
 
-    
+    # Vertices representing jobs.
+    J = 1:jobs_count
+
+    # Vertices representing machines.
+    M = 1:machines_count
+
+    # Bipartite graph representation.
+    graph = []
+
+    for i in M
+        for j in J
+            push!(graph, (i, j))
+        end
+    end
+
+    println(graph)
+
+    # J' = [j for j in J]
+    # M' = [i for i in M]
+    # F = []
+
+    while length(J) > 0
+        model = Model(GLPK.Optimizer)
+
+        @variable(model, x[M, J] >= 0)
+
+        @objective(model, Min, sum(costs[i, j] * x[i, j] for (i, j) in graph))
+
+    end
 end
 
 function run(path)
-    problem_data = parse_file(path)
-    solve_gap(problem_data)
+    problems = parse_file(path)
+
+    for p in problems
+        solve_gap(p)
+    end
 end
 
--- list_3/parser.jl
+++ list_3/parser.jl
@@ -27,39 +27,52 @@ function parse_file(path)
 
     problems_set = []
 
-    while idx <= length(data)
+    while idx < length(data)
         machines_count = data[idx][1]
         jobs_count = data[idx][2]
 
         idx += 1
-        costs = []
+        costs = Array{Int64}(undef, machines_count, jobs_count)
+        j = 1
+    
         for i in idx:(idx + machines_count - 1)
             tmp = deepcopy(data[i])
 
-            while length(data[i + 1]) !== jobs_count && length(tmp) < jobs_count
-                append!(tmp, data[i + 1])
-                deleteat!(data, i + 1)
-            end
-            push!(costs, tmp) 
-        end
-
-        idx += machines_count - 1
-        resources = []
-        for i in idx:(idx + machines_count - 1)
-            tmp = deepcopy(data[i])
-
-            while length(data[i + 1]) != jobs_count && length(tmp) < jobs_count
-                append!(tmp, data[i + 1])
+            while length(tmp) < jobs_count
+                append!(tmp, deepcopy(data[i + 1]))
                 deleteat!(data, i + 1)
             end
 
-            push!(resources, tmp)
+            for (k, value) in pairs(IndexStyle(tmp), tmp)
+                costs[j, k] = value
+            end
+
+            j += 1
         end
 
         idx += machines_count
-        capacities = data[idx + 1]
 
-        idx += 2
+        resources = Array{Int64}(undef, machines_count, jobs_count)
+        j = 1
+        for i in idx:(idx + machines_count - 1)
+            tmp = deepcopy(data[i])
+            
+            while length(tmp) < jobs_count
+                append!(tmp, deepcopy(data[i + 1]))
+                deleteat!(data, i + 1)
+            end
+
+            for (k, value) in pairs(IndexStyle(tmp), tmp)
+                resources[j, k] = value
+            end
+
+            j += 1
+        end
+        idx += machines_count
+
+        capacities = deepcopy(data[idx])
+
+        idx += 1
 
         push!(problems_set, (machines_count, jobs_count, costs, resources, capacities))
 
